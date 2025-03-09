06c3a02dd1fd988b73f02642ba3f12fe7588af99
MaksimSinicbIn <dzigen1986@gmail.com>
2025-03-09 18:50:23 +0700

    Feat: improve functionality of the button

--- src/common/components/button/button.tsx
+++ src/common/components/button/button.tsx
@@ -1,11 +1,20 @@
 import { ComponentPropsWithoutRef, ElementType } from 'react'
 import s from './button.module.scss'
+import clsx from 'clsx'
+
+type ButtonVarriant = 'primary' | 'secondary' | 'outlined'
 
 type Props<T extends ElementType = 'button'> = {
   as?: T
+  fullWidth?: boolean
+  variant?: ButtonVarriant
+  className?: string
 } & ComponentPropsWithoutRef<T>
 
-export const Button = (props: Props) => {
-  const { children } = props
-  return <button className={s.primary}>{children}</button>
+export const Button = <T extends ElementType = 'button'>(props: Props<T>) => {
+  const { as, variant = 'primary', className, fullWidth, ...restProps } = props
+
+  const Component = as ?? 'button'
+
+  return <Component className={clsx(s[variant], s.button, fullWidth && s.fullWidth, className)} {...restProps} />
 }
