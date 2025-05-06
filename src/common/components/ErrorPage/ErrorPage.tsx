5b0b9c076d9376cb291383931e5d395b097930e4
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-06 20:24:22 +0700

    Feat: add Error page

---
+++ src/common/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,19 @@
+import wrongDoor from '@/assets/images/wrongDoor.webp'
+
+export const ErrorPage = () => {
+  return (
+    <div className='error-page'>
+      <div style={{ marginTop: '10%', textAlign: 'center' }}>
+        <h1>Oops!</h1>
+        <p>You got the wrong door, buddy!</p>
+        <img style={{ paddingTop: '2%' }} src={wrongDoor} alt='drujok_pirojok' />
+        <p style={{ paddingTop: '2%', fontSize: '18px' }}>
+          Try click{' '}
+          <a href={'http://localhost:5173'} target={'_blank'}>
+            <span style={{ color: 'violet' }}>here!</span>
+          </a>
+        </p>
+      </div>
+    </div>
+  )
+}
