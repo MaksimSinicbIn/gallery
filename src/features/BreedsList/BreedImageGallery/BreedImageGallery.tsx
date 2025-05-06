77e9a047fea68f4e46b2481b8ad834bdde7d630d
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-06 20:30:46 +0700

    Refactor: replace props with useParams for data fetching

--- src/features/BreedsList/BreedImageGallery/BreedImageGallery.tsx
+++ src/features/BreedsList/BreedImageGallery/BreedImageGallery.tsx
@@ -1,28 +1,19 @@
 import { useGetImagesByBreedQuery } from '@/app/baseApi'
-import s from './BreedImageGallery.module.scss'
-import { Button } from '@/common/components/Button/Button'
+import { useParams } from 'react-router'
 import { BreedImageCard } from './BreedImageCard/BreedImageCard'
+import s from './BreedImageGallery.module.scss'
 
-type Props = {
-  selectedBreed: string | null
-  onBack: () => void
-}
+export const BreedImageGallery = () => {
+  const params = useParams()
+  const breed = params.breed
+  const { data: images = [], isLoading, error } = useGetImagesByBreedQuery(breed || '', { skip: !breed })
 
-export const BreedImageGallery = ({ selectedBreed, onBack }: Props) => {
-  const {
-    data: images = [],
-    isLoading,
-    error,
-  } = useGetImagesByBreedQuery(selectedBreed || '', { skip: !selectedBreed })
-
+  if (!breed) return <div>No breed selected</div>
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>Error: {JSON.stringify(error)}</div>
 
   return (
     <div>
-      <div style={{ margin: `10px 10px` }}>
-        <Button onClick={() => onBack()}>← Назад к списку</Button>
-      </div>
       <div className={s.card}>{images?.map((imageUrl) => <BreedImageCard key={imageUrl} imageUrl={imageUrl} />)}</div>
     </div>
   )
