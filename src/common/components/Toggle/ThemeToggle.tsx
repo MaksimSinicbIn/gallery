986f3455c09ac6d5e3fcf0c03e859207a68a12f5
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-12 21:16:52 +0700

    Feat: add ThemeToggle using lucide-react

---
+++ src/common/components/Toggle/ThemeToggle.tsx
@@ -0,0 +1,23 @@
+import { useAppDispatch, useAppSelector } from '@/common/hooks'
+import { selectThemeMode, toggleTheme } from '@/app/appSlice'
+import { Button } from '@/common/components/Button/Button'
+import { Moon, Sun } from 'lucide-react'
+import s from './ThemeToggle.module.scss'
+
+export const ThemeToggle = () => {
+  const dispatch = useAppDispatch()
+
+  const currentTheme = useAppSelector(selectThemeMode)
+
+  const changeThemeHandler = () => {
+    dispatch(toggleTheme())
+  }
+
+  return (
+    <div className={s['toggle-container']}>
+      <Button variant='icon' onClick={changeThemeHandler}>
+        {currentTheme === 'dark' ? <Moon /> : <Sun />}
+      </Button>
+    </div>
+  )
+}
--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import { Button } from '../Button/Button'
-import s from './header.module.scss'
+import { ThemeToggle } from '@/common/components/Toggle/ThemeToggle'
+import s from './Header.module.scss'
 
 export const Header = () => {
-  const [open, setOpen] = useState(false)
-
-  const openMenu = () => setOpen(!open)
-  console.log(open)
-
   return (
     <div className={s.header}>
-      <Button variant='secondary' onClick={openMenu}>
-        Menu
-      </Button>
+      <ThemeToggle />
     </div>
   )
 }
