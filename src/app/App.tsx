8a9f54eb78ab2753ed5592115785f9de14e90f15
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-15 22:31:49 +0700

    Refactor: add route, change HomePage with routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,16 @@ import { Navigate, Route, Routes } from 'react-router'
 import { ErrorPage } from '@/common/components/ErrorPage/ErrorPage'
 import { Header } from '@/common/components/Header/Header'
 import { HomePage } from '@/common/components/HomePage/HomePage'
-import { BreedImageGallery } from '@/features/BreedsList/BreedImageGallery/BreedImageGallery'
-import { BreedsList } from '@/features/BreedsList/BreedsList'
+import { BreedImageGallery } from '@/features/breeds/BreedImageGallery/BreedImageGallery'
+import { BreedsList } from '@/features/breeds/BreedsList/BreedsList'
 import { RandomImageGallery } from '@/features/RandomImages/RandomImageGallery'
+import { SubBreedImageGallery } from '@/features/breeds/SubBreedImageGallery/SubBreedImageGallery'
 
 export const PATH = {
   HOME: '/home',
   BREEDS: '/breeds',
   BREED_DETAILS: '/breeds/:breed',
+  SUB_BREED: '/breeds/:breed/:subBreed',
   RANDOM: '/random',
   NOT_FOUND: '/404',
 } as const
@@ -25,6 +27,7 @@ export const App = () => {
           <Route path={PATH.RANDOM} element={<RandomImageGallery />} />
           <Route path={PATH.BREEDS} element={<BreedsList />} />
           <Route path={PATH.BREED_DETAILS} element={<BreedImageGallery />} />
+          <Route path={PATH.SUB_BREED} element={<SubBreedImageGallery />} />
           <Route path={PATH.NOT_FOUND} element={<ErrorPage />} />
           <Route path='*' element={<Navigate to={PATH.NOT_FOUND} />} />
         </Routes>
--- src/common/components/HomePage/HomePage.tsx
+++ src/common/components/HomePage/HomePage.tsx
@@ -12,10 +12,10 @@ export const HomePage = () => {
       </video>
       Welcome to Dogs Gallery! Here you can see images of the dog breeds you're interested in!
       <div className={s.buttonSpace}>
-        <Button variant='primary' title='10 random images'>
+        <Button variant='outlined' title='10 random images'>
           <Link to={PATH.RANDOM}>View 10 random dog images</Link>
         </Button>
-        <Button variant='primary' title='Chose breed'>
+        <Button variant='outlined' title='Chose breed'>
           <Link to={PATH.BREEDS}>Choose a breed you're interested in</Link>
         </Button>
       </div>
