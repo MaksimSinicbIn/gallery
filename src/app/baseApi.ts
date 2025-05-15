f47dddbca64e86e82264111e31cf781a9510695e
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-15 22:18:53 +0700

    Feat: Improve getBreedsList query to handle subBreeds processing, add getImagesBySubBreed query to baseApi, add typing

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,7 +1,17 @@
 import { BREED_NAME_CORRECTIONS, BreedKey } from '@/common/constants'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-export type GroupedBreeds = Record<string, string[]>
+export interface BreedsResponse {
+  message: Record<string, string[]>
+  status: string
+}
+
+export interface BreedItem {
+  name: string
+  subBreeds: string[]
+}
+
+export type GroupedBreeds = Record<string, BreedItem[]>
 
 export const baseAPi = createApi({
   reducerPath: 'dogsApi',
@@ -17,15 +27,20 @@ export const baseAPi = createApi({
       query: (count) => `breeds/image/random/${count}`,
       transformResponse: (response: { message: string[] }) => response.message,
     }),
-    getBreedsList: builder.query<string[], void>({
+    getBreedsList: builder.query<BreedItem[], void>({
       query: () => `breeds/list/all`,
-      transformResponse: (response: { message: GroupedBreeds }) =>
-        Object.keys(response.message).map(
-          (breed) => BREED_NAME_CORRECTIONS[breed as BreedKey] || breed.charAt(0).toUpperCase() + breed.slice(1)
-        ),
+      transformResponse: (response: BreedsResponse) =>
+        Object.entries(response.message).map(([breed, subBreeds]) => ({
+          name: BREED_NAME_CORRECTIONS[breed as BreedKey] || breed.charAt(0).toUpperCase() + breed.slice(1),
+          subBreeds: subBreeds.map((sub) => sub.charAt(0).toUpperCase() + sub.slice(1)),
+        })),
     }),
     getImagesByBreed: builder.query<string[], string>({
       query: (breed) => `breed/${breed}/images`,
+      transformResponse: (response: { message: string[] }) => response.message.slice(0, 10),
+    }),
+    getImagesBySubBreed: builder.query<string[], { breedName: string; subBreedName: string }>({
+      query: ({ breedName, subBreedName }) => `breed/${breedName}/${subBreedName}/images`,
       transformResponse: (response: { message: string[] }) => response.message.slice(0, 10),
     }),
   }),
@@ -36,4 +51,5 @@ export const {
   useGetMultipleDogImagesQuery,
   useGetBreedsListQuery,
   useGetImagesByBreedQuery,
+  useGetImagesBySubBreedQuery,
 } = baseAPi
