b47e3e456727c3e8c4b1b5591ac1ba4a63abc3e8
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-15 22:26:23 +0700

    Feat: add SubBreedImageGallery component

---
+++ src/features/breeds/SubBreedImageGallery/SubBreedImageGallery.tsx
@@ -0,0 +1,25 @@
+import { useGetImagesBySubBreedQuery } from '@/app/baseApi'
+import { ImageCard } from '@/common/components/ImageCard/ImageCard'
+import { useParams } from 'react-router'
+import s from './SubBreedImageGallery.module.scss'
+
+export const SubBreedImageGallery = () => {
+  const params = useParams()
+  const breed = params.breed
+  const subBreed = params.subBreed
+  const {
+    data: images = [],
+    isLoading,
+    error,
+  } = useGetImagesBySubBreedQuery({ breedName: breed!, subBreedName: subBreed! }, { skip: !breed || !subBreed })
+
+  if (!breed) return <div>No breed selected</div>
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>Error: {JSON.stringify(error)}</div>
+
+  return (
+    <div>
+      <div className={s.card}>{images?.map((imageUrl) => <ImageCard key={imageUrl} imageUrl={imageUrl} />)}</div>
+    </div>
+  )
+}
