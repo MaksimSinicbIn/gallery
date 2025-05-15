241ce768b3fd3cb63119fe44576946756f9744a0
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-15 22:25:43 +0700

    Feat: add SubBreedList component

---
+++ src/features/breeds/SubBreedList/SubBreedList.tsx
@@ -0,0 +1,25 @@
+import { BreedItem } from '@/app/baseApi'
+import s from './SubBreedList.module.scss'
+import { Link } from 'react-router'
+import { normalizeBreedName } from '@/common/utils'
+
+type Props = {
+  breed: BreedItem
+}
+
+export const SubBreedList = ({ breed }: Props) => {
+  return (
+    <div className={s.dropdown}>
+      <details>
+        <summary>{breed.name}</summary>
+        <ul className={s.subBreedsList}>
+          {breed.subBreeds.map((subBreed) => (
+            <li key={subBreed}>
+              <Link to={`/breeds/${normalizeBreedName(breed.name)}/${normalizeBreedName(subBreed)}`}>{subBreed}</Link>
+            </li>
+          ))}
+        </ul>
+      </details>
+    </div>
+  )
+}
