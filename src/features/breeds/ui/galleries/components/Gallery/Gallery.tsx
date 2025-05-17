6fb684d3152aef44fcf5fb51200c443aca516da3
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-17 07:09:40 +0700

    Refactor: update Gallery component with masonry layout

--- src/features/breeds/ui/galleries/components/Gallery/Gallery.tsx
+++ src/features/breeds/ui/galleries/components/Gallery/Gallery.tsx
@@ -1,10 +1,26 @@
-import { ImageCard } from '../ImageCard/ImageCard'
+import Masonry from 'react-masonry-css'
 import s from './Gallery.module.scss'
+import { ImageCard } from '../ImageCard/ImageCard'
 
 type Props = {
   images: string[]
 }
 
 export const Gallery = ({ images }: Props) => {
-  return <div className={s.card}>{images?.map((imageUrl) => <ImageCard key={imageUrl} imageUrl={imageUrl} />)}</div>
+  const breakpointColumnsObj = {
+    default: 4, // 4 колонки по умолчанию
+    1100: 3, // 3 колонки при ширине ≤1100px
+    700: 2, // 2 колонки при ширине ≤700px
+    500: 1, // 1 колонка на мобильных
+  }
+
+  return (
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className={s['my-masonry-grid']} // Класс для контейнера
+      columnClassName={s['my-masonry-grid_column']} // Класс для колонок
+    >
+      {images?.map((imageUrl) => <ImageCard key={imageUrl} imageUrl={imageUrl} />)}
+    </Masonry>
+  )
 }
