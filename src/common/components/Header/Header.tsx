6b67d9fab7021f53327c510c4101a15ef3c98ee7
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-22 13:17:45 +0700

    Refactor (header): add dynamic style changes on scroll

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,19 +1,35 @@
 import { ThemeToggle } from '@/common/components/Toggle/ThemeToggle'
-import s from './Header.module.scss'
 import { Home, MoveLeft } from 'lucide-react'
+import clsx from 'clsx'
 import { useAppNavigate } from '@/common/hooks'
 import { Button } from '../Button/Button'
 import { useLocation } from 'react-router'
+import { useState, useEffect } from 'react'
+import s from './Header.module.scss'
 
 export const Header = () => {
   const { goHome, goBack } = useAppNavigate()
 
   const location = useLocation()
 
+  const [hasScrolled, setHasScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0
+      if (isScrolled !== hasScrolled) {
+        setHasScrolled(isScrolled)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll, { passive: true })
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [hasScrolled])
+
   return (
     <header className={s.headerSide}>
       <nav aria-label='Control'>
-        <ul className={s.headerRight}>
+        <ul className={clsx(s.headerRight, hasScrolled && s.headerScrolled)}>
           <li>
             <ThemeToggle />
           </li>
@@ -26,7 +42,7 @@ export const Header = () => {
       </nav>
       {location.pathname !== '/home' && (
         <nav aria-label='Back'>
-          <ul className={s.headerLeft}>
+          <ul className={clsx(s.headerLeft, hasScrolled && s.headerScrolled)}>
             <li>
               <Button aria-label='Back' title='Previous page' variant='icon' onClick={goBack}>
                 <MoveLeft />
