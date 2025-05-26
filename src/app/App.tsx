997a4f42a1df041264fde605a6a1f2366d7b0ab9
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-26 23:52:11 +0700

    Refactor: update routing logic, global styles and add fullscreen layout classes, remove unused files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router'
+import { Navigate, Route, Routes, useLocation } from 'react-router'
 import { Header } from '@/common/components/Header/Header'
 import { ErrorPage } from '@/common/components/ErrorPage/ErrorPage'
 import { HomePage } from '@/common/components/HomePage/HomePage'
@@ -6,6 +6,7 @@ import { BreedsList } from '@/features/breeds/ui/lists/BreedsList/BreedsList'
 import { BreedImageGallery } from '@/features/breeds/ui/galleries/BreedImageGallery/BreedImageGallery'
 import { RandomImageGallery } from '@/features/breeds/ui/galleries/RandomImages/RandomImageGallery'
 import { SubBreedImageGallery } from '@/features/breeds/ui/galleries/SubBreedImageGallery/SubBreedImageGallery'
+import clsx from 'clsx'
 
 export const PATH = {
   HOME: '/home',
@@ -17,21 +18,32 @@ export const PATH = {
 } as const
 
 export const App = () => {
+  const location = useLocation()
+
+  const isHomePage = location.pathname === PATH.HOME
+
   return (
     <>
-      <Header />
-      <main className='container'>
-        <Routes>
-          <Route path={PATH.HOME} element={<HomePage />} />
-          <Route path='/' element={<Navigate to={PATH.HOME} />} />
-          <Route path={PATH.RANDOM} element={<RandomImageGallery />} />
-          <Route path={PATH.BREEDS} element={<BreedsList />} />
-          <Route path={PATH.BREED_DETAILS} element={<BreedImageGallery />} />
-          <Route path={PATH.SUB_BREED} element={<SubBreedImageGallery />} />
-          <Route path={PATH.NOT_FOUND} element={<ErrorPage />} />
-          <Route path='*' element={<Navigate to={PATH.NOT_FOUND} />} />
-        </Routes>
-      </main>
+      {!isHomePage && <Header />}
+      <Routes>
+        <Route path={PATH.HOME} element={<HomePage />} />
+        <Route path='/' element={<Navigate to={PATH.HOME} />} />
+        <Route
+          path='*'
+          element={
+            <main className={clsx('container', isHomePage ? 'fullScreen' : 'withHeader')}>
+              <Routes>
+                <Route path={PATH.RANDOM} element={<RandomImageGallery />} />
+                <Route path={PATH.BREEDS} element={<BreedsList />} />
+                <Route path={PATH.BREED_DETAILS} element={<BreedImageGallery />} />
+                <Route path={PATH.SUB_BREED} element={<SubBreedImageGallery />} />
+                <Route path={PATH.NOT_FOUND} element={<ErrorPage />} />
+                <Route path='*' element={<Navigate to={PATH.NOT_FOUND} />} />
+              </Routes>
+            </main>
+          }
+        />
+      </Routes>
     </>
   )
 }
