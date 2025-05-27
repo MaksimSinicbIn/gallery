d1b641ab68944b99ed2a2e06ea47aca8994ea2e6
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-27 17:46:01 +0700

    Refactor: improve HomePage html structure and styles, change scss variables

--- src/common/components/HomePage/HomePage.tsx
+++ src/common/components/HomePage/HomePage.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/common/components/Button/Button'
-import homePageImage from '@/assets/images/homePageImage.webp'
 import s from './HomePage.module.scss'
 import { Link } from 'react-router'
 import { PATH } from '@/app/App'
@@ -7,19 +6,25 @@ import { PATH } from '@/app/App'
 export const HomePage = () => {
   return (
     <section className={s.pageContainer} aria-labelledby='section-title section-description'>
-      <img className={s.image} src={homePageImage} alt='Dog image' />
       <div className={s.pageInfo}>
-        <h1 id='section-title'>Dogs Gallery</h1>
-        <p id='section-description'>
-          Welcome to Dogs Gallery! Here you can see images of the dog breeds you're interested in!
-        </p>
+        <h1 className={s.pageTitle} id='section-title'>
+          Dogs Gallery
+        </h1>
+        <div className={`${s.textBlock} ${s.pageText}`} id='section-description'>
+          <span>Welcome to Dogs Gallery!</span>
+          <span>Here you can see images of the dog breeds you're interested in!</span>
+        </div>
         <div className={s.buttonSpace}>
-          <Button variant='outlined' title='10 random images'>
-            <Link to={PATH.RANDOM}>View 10 random dog images</Link>
-          </Button>
-          <Button variant='outlined' title='Chose breed'>
-            <Link to={PATH.BREEDS}>Choose a breed you're interested in</Link>
-          </Button>
+          <Link to={PATH.RANDOM}>
+            <Button variant='outlined' title='10 random images'>
+              View random dog
+            </Button>
+          </Link>
+          <Link to={PATH.BREEDS}>
+            <Button variant='outlined' title='Chose breed'>
+              Choose a breed
+            </Button>
+          </Link>
         </div>
       </div>
     </section>
