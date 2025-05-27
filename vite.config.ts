d1b4358bc4368a06b5884d55013884e3115b7951
MaksimSinicbIn <dzigen1986@gmail.com>
2025-05-28 00:26:52 +0700

    Feat: configure Service Worker image caching

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /https:\/\/images\.dog\.ceo\/.*\.(jpg|jpeg|png|webp)/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'dog-images-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`,
