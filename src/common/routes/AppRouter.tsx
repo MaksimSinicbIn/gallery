25ab7214bd6740f277495a9c58ff87e14c8fea30
MaksimSinicbIn <dzigen1986@gmail.com>
2025-06-02 18:43:12 +0700

    Refactor (routing): simplify routing, apply MainLayout to all routes except HomePage and ErrorPage

--- src/common/routes/AppRouter.tsx
+++ src/common/routes/AppRouter.tsx
@@ -2,48 +2,33 @@ import { BreedImageGallery } from '@/features/breeds/ui/galleries/BreedImageGall
 import { RandomImageGallery } from '@/features/breeds/ui/galleries/RandomImages/RandomImageGallery'
 import { SubBreedImageGallery } from '@/features/breeds/ui/galleries/SubBreedImageGallery/SubBreedImageGallery'
 import { BreedsList } from '@/features/breeds/ui/lists/BreedsList/BreedsList'
+import { MainLayout } from '../components/Layout/MainLayout'
 import { ErrorPage } from '../components/ErrorPage/ErrorPage'
 import { HomePage } from '../components/HomePage/HomePage'
-import { Header } from '../components/Header/Header'
-import { Routes, Route, Navigate, useLocation } from 'react-router'
-import clsx from 'clsx'
+import { Routes, Route, Navigate } from 'react-router'
 
 export const PATH = {
   HOME: '/home',
+  RANDOM: '/random',
   BREEDS: '/breeds',
   BREED_DETAILS: '/breeds/:breed',
   SUB_BREED: '/breeds/:breed/:subBreed',
-  RANDOM: '/random',
-  NOT_FOUND: '/404',
+  NOT_FOUND: '/not_found',
 } as const
 
 export const AppRouter = () => {
-  const location = useLocation()
-
-  const isHomePage = location.pathname === PATH.HOME
-
   return (
-    <>
-      {!isHomePage && <Header />}
-      <Routes>
-        <Route path={PATH.HOME} element={<HomePage />} />
-        <Route path='/' element={<Navigate to={PATH.HOME} />} />
-        <Route
-          path='*'
-          element={
-            <div className={clsx('container', 'withHeader')}>
-              <Routes>
-                <Route path={PATH.RANDOM} element={<RandomImageGallery />} />
-                <Route path={PATH.BREEDS} element={<BreedsList />} />
-                <Route path={PATH.BREED_DETAILS} element={<BreedImageGallery />} />
-                <Route path={PATH.SUB_BREED} element={<SubBreedImageGallery />} />
-                <Route path={PATH.NOT_FOUND} element={<ErrorPage />} />
-                <Route path='*' element={<Navigate to={PATH.NOT_FOUND} />} />
-              </Routes>
-            </div>
-          }
-        />
-      </Routes>
-    </>
+    <Routes>
+      <Route path={PATH.HOME} element={<HomePage />} />
+      <Route path='/' element={<Navigate to={PATH.HOME} />} />
+      <Route element={<MainLayout />}>
+        <Route path={PATH.RANDOM} element={<RandomImageGallery />} />
+        <Route path={PATH.BREEDS} element={<BreedsList />} />
+        <Route path={PATH.BREED_DETAILS} element={<BreedImageGallery />} />
+        <Route path={PATH.SUB_BREED} element={<SubBreedImageGallery />} />
+        <Route path={PATH.NOT_FOUND} element={<ErrorPage />} />
+      </Route>
+      <Route path='*' element={<Navigate to={PATH.NOT_FOUND} />} />
+    </Routes>
   )
 }
