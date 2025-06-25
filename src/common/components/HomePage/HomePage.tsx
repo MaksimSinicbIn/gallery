8621d12050b3343ea9fe6f99417ebf2c0f73c771
MaksimSinicbIn <dzigen1986@gmail.com>
2025-06-25 18:57:07 +0700

    Refactor (HomePage): reorganize page structure and update styles

--- src/common/components/HomePage/HomePage.tsx
+++ src/common/components/HomePage/HomePage.tsx
@@ -11,13 +11,14 @@ export const HomePage = () => {
           Dogs Gallery
         </h1>
         <div className={`${s.textBlock} ${s.text}`} id='section-description'>
-          <span>Welcome to Dogs Gallery!</span>
-          <span>Here you can see images of the dog breeds you're interested in!</span>
+          <p>Find your breed</p>
+          <p>Browse curated collection</p>
+          <p>Discover exceptional dogs worldwide</p>
         </div>
         <div className={s.buttonSpace}>
           <Link to={PATH.RANDOM}>
-            <Button variant='outlined' title='10 random images'>
-              View random dog
+            <Button variant='outlined' title='20 random images'>
+              View random dogs
             </Button>
           </Link>
           <Link to={PATH.BREEDS}>
