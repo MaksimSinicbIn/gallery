84d6d8f49ea7496cf5275da34e81c86af52d0562
MaksimSinicbIn <dzigen1986@gmail.com>
2025-08-08 13:18:25 +0700

    Feat (HomePage): add WebP fallback for older devices

--- src/common/components/HomePage/HomePage.tsx
+++ src/common/components/HomePage/HomePage.tsx
@@ -1,6 +1,9 @@
-import homePageImage1 from '@/assets/images/homePageImage1.avif'
-import homePageImage2 from '@/assets/images/homePageImage2.avif'
-import homePageImage3 from '@/assets/images/homePageImage3.avif'
+import homePageImage1Avif from '@/assets/images/homePageImage1.avif'
+import homePageImage1Webp from '@/assets/images/homePageImage1.webp'
+import homePageImage2Avif from '@/assets/images/homePageImage2.avif'
+import homePageImage2Webp from '@/assets/images/homePageImage2.webp'
+import homePageImage3Avif from '@/assets/images/homePageImage3.avif'
+import homePageImage3Webp from '@/assets/images/homePageImage3.webp'
 import { PATH } from '@/common/routes/AppRouter'
 import { Link } from 'react-router'
 import s from './HomePage.module.scss'
@@ -9,7 +12,10 @@ import clsx from 'clsx'
 export const HomePage = () => {
   return (
     <section className={s.gridContainer} aria-labelledby='section-title' aria-describedby='section-description'>
-      <img className={s.image} src={homePageImage1} alt='Dogs main image' />
+      <picture>
+        <source srcSet={homePageImage1Avif} type='image/avif' />
+        <img className={s.image} src={homePageImage1Webp} alt='Dogs main image' />
+      </picture>
       <div className={s.textBlock} tabIndex={0}>
         <h1 className={s.title} id='section-title'>
           Dogs Gallery
@@ -20,15 +26,21 @@ export const HomePage = () => {
           <p>Discover exceptional dogs from around the world</p>
         </div>
       </div>
-      <Link to={PATH.RANDOM} className={s.imageLink}>
+      <Link to={PATH.RANDOM} className={s.imageLink} aria-label='View random dogs images'>
         <div className={s.imageWrapper}>
-          <img className={s.image} src={homePageImage2} alt='Dogs image for random' />
+          <picture>
+            <source srcSet={homePageImage2Avif} type='image/avif' />
+            <img className={s.image} src={homePageImage2Webp} alt='Dogs image for random' />
+          </picture>
           <span className={s.imageOverlay}>Random Breed</span>
         </div>
       </Link>
       <Link to={PATH.BREEDS} className={s.imageLink} aria-label='Choose a dog breed'>
         <div className={s.imageWrapper}>
-          <img className={s.image} src={homePageImage3} alt='Dogs image for chose breed' />
+          <picture>
+            <source srcSet={homePageImage3Avif} type='image/avif' />
+            <img className={s.image} src={homePageImage3Webp} alt='Dogs image for chose breed' />
+          </picture>
           <span className={s.imageOverlay}>Chose Breed</span>
         </div>
       </Link>
